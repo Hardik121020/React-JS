e23434b4391edc44fd507bab4190b357809a9e0a
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2020-12-27 19:57:23 +0530

    Add files via upload

---
+++ About-Us/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import AboutUs from './component/AboutUs';
+import Joinbar from './component/Joinbar';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <AboutUs />
+    <Joinbar />
+    
+    </>
+  );
+}
+
+export default App;
