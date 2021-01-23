7e3ec3072cbfc73ee0744a1f73b13d1f65152a26
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2021-01-23 15:44:45 +0530

    Add files via upload

---
+++ Courses-Master/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import CourseBar from './component/CourseBar';
+
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <CourseBar />
+    
+    </>
+  );
+}
+
+export default App;
