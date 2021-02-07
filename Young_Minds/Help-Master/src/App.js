c94745688b605c68f6b3f6460bbbea0a2dde775a
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2021-02-07 16:29:01 +0530

    Add files via upload

---
+++ Young_Minds/Help-Master/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import HelpBar from './component/HelpBar';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <HelpBar />
+    
+    </>
+  );
+}
+
+export default App;
---
+++ Young_Minds/Help-Master/src/component/Accordion.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Accordion.css';
+
+
+const Accordion = ({ title , active , setActive , content}) => {
+    return(
+        <div className="accordion">
+            <div className="accordionheading">
+                <div className="container">
+                    <p>{title}</p>
+                    <span onClick={() => setActive(title)}>
+                        {active === title ? "X" : "|||"}
+                    </span>
+                </div>
+            </div>
+
+            <div className={(active === title ? "show" : "") + " accordionContent"}>
+                <div className="container">
+                    <p className='auto-scrollbar'>
+                        {content}
+                    </p>
+                
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+export default Accordion;
