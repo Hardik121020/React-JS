ba31ad8749d9aad499e0064e783ae424c154ad89
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2021-02-13 18:55:26 +0530

    Add files via upload

---
+++ Young_Minds/Women_in_tech/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Text from './component/Text';
+import WomenInTech from './component/WomenInTech';
+
+function App() {
+  return (
+    <>
+    
+    <Text />
+    <WomenInTech />
+    
+    </>
+  );
+}
+
+export default App;
