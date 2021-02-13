86bd5409ae50733113135ba5a3dffbbbeaa9671b
Hardik121020 <55340731+Hardik121020@users.noreply.github.com>
2021-02-13 19:04:49 +0530

    Add files via upload

---
+++ Young_Minds/Join_Our_Community/src/component/Text.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Text.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import './font/css/font-awesome.css';
+
+
+
+
+const Text = () => {
+    return (
+        <>
+
+            <div className="container-fluid cbox">
+                <div className="row crow1">
+                    <div className="col-md-4 cobox">
+                        <h1 id="text1">Join Our Community</h1>
+                    </div>
+                    <div className="col-md-5 arrow">
+                        <a href="#" >
+                        <i class="fa fa-angle-left fa-3x ar" href="#"></i>
+                        </a>
+                        <a href="#" >
+                        <i class="fa fa-angle-right fa-3x" href="#" ></i>
+                        </a>
+                    </div>
+                    <div className="col-md-2 cobox" id="dropdown">
+                   
+                            <Dropdown>
+                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                                    COMMUNITY
+                            </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">SUPPORT</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">EXPLORE</Dropdown.Item>
+
+                                </Dropdown.Menu>
+                            </Dropdown>
+
+                        </div>
+                    </div>
+                </div>
+
+
+        </>
+    );
+}
+export default Text;
